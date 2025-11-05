1aef7d79e734468840207c2ae1bac14881c47a92
WenyiY <chrisyin@udel.edu>
2025-11-05 14:38:44 -0800

    Make the frontend works without APIs

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,18 +1,23 @@
 /* This is the Page 2 after user clicked in Home page, contains Map + POI + sidebar */
 
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import MapView from "../components/MapView";
 import Sidebar from "../components/Sidebar";
 import DetailsModal from "../components/DetailsModal";
+import "./Result.css";
 
-export default function Results() {
+export default function Result() {
     const { state } = useLocation(); // contains start/end data
+    const [selectedPOI, setSelectedPOI] = useState(null);
+
     return (
-        <div className="results-page">
-            <Sidebar />
-            <MapView start={state?.start} end={state?.end} />
-            <DetailsModal />
+        <div style={{ display: "flex", height: "100vh", position: "relative" }}>
+            <Sidebar setSelectedPOI={setSelectedPOI} />
+            <MapView start={state?.start} end={state?.end} setSelectedPOI={setSelectedPOI} />
+            {selectedPOI && (
+                <DetailsModal poi={selectedPOI} onClose={() => setSelectedPOI(null)} />
+            )}
         </div>
     );
 }
