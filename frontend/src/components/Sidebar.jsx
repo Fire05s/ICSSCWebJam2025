b5ced877dca9f58331a2dbcab3bc8f3ed1e0de3d
jedi-narwhal <soniasnlep@yahoo.com>
2025-11-05 19:25:22 -0800

    added 2 buttons to sidebar and created sidebar.css file

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "sidebar.css"
 const POIs = [
     { id: 1, name: "Art Museum", type: "landmark", image: "https://image" },
     { id: 2, name: "Sunset Café", type: "restaurant", image: "https://image" },
@@ -8,13 +8,17 @@ const POIs = [
 export default function Sidebar({ setSelectedPOI }) {
     return (
         <div style={{
-            width: "280px",
+            width: "430px",
             borderRight: "1px solid #ccc",
             overflowY: "auto",
             padding: "10px",
             backgroundColor: "#f8f8f8"
         }}>
             <h2>Points of Interest</h2>
+            <div>
+             <button type="button">Resturants</button>
+            <button type="button">Landmarks</button>
+            </div>
             {POIs.map((poi) => (
                 <div
                     key={poi.id}
@@ -25,9 +29,11 @@ export default function Sidebar({ setSelectedPOI }) {
                     <div>
                         <strong>{poi.name}</strong>
                         <p style={{ margin: 0, fontSize: "12px", color: "#666" }}>{poi.type}</p>
+
                     </div>
                 </div>
             ))}
         </div>
     );
 }
+
